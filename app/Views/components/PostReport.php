beccd0048cd9e928cf9730a8e24e6de937115eaf
Nutthapon Saefong <73591319+nut1245@users.noreply.github.com>
2022-03-31 21:56:37 +0700

    page-reportpost

---
+++ app/Views/components/PostReport.php
@@ -0,0 +1,121 @@
+<div class="card" >
+    <div class="card-body" >
+        <h3 class="card-title" >โพสต์ที่ถูกรายงาน</h3>
+        <div class="row row-cols-1 row-cols-md-3 g-4 " >
+            <div class="col" >
+                <div class="card" >
+                    <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg" class="card-img-post " alt="...">
+                    <div class="card-body" id="card-color">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="col-sm-12 ">
+                            <center>
+                                <a href="#" class="btn btn-primary btn-update" data-bs-toggle="modal" data-bs-target="#update3">
+                                    &nbsp;<i class="mdi mdi-eye"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-update" data-bs-toggle="modal" data-bs-target="#update3">
+                                    &nbsp;<i class="mdi mdi-delete"></i>
+                                </a>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://youimg1.tripcdn.com/target/100g1f000001gqxhkE51A.jpg?proc=source%2Ftrip" class="card-img-post" alt="...">
+                    <div class="card-body" id="card-color">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                        <div class="col-sm-12 ">
+                            <center>
+                                <a href="#" class="btn btn-primary btn-update" data-bs-toggle="modal" data-bs-target="#update3">
+                                    &nbsp;<i class="mdi mdi-eye"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-update" data-bs-toggle="modal" data-bs-target="#update3">
+                                    &nbsp;<i class="mdi mdi-delete"></i>
+                                </a>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://dimg04.c-ctrip.com/images/0M75l120008ytj8h9DB92.png_.webp" class="card-img-post" alt="...">
+                    <div class="card-body" id="card-color">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                        <div class="col-sm-12 ">
+                            <center>
+                                <a href="#" class="btn btn-primary btn-update" data-bs-toggle="modal" data-bs-target="#update3">
+                                    &nbsp;<i class="mdi mdi-eye"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-update" data-bs-toggle="modal" data-bs-target="#update3">
+                                    &nbsp;<i class="mdi mdi-delete"></i>
+                                </a>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://th.jobsdb.com/th-th/wp-content/uploads/sites/3/2021/01/%E0%B9%80%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%A2%E0%B8%A7%E0%B9%82%E0%B8%84%E0%B8%A7%E0%B8%B4%E0%B8%94_800x500-1.png" class="card-img-post" alt="...">
+                    <div class="card-body" id="card-color">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                        <div class="col-sm-12 ">
+                            <center>
+                                <a href="#" class="btn btn-primary btn-update" data-bs-toggle="modal" data-bs-target="#update3">
+                                    &nbsp;<i class="mdi mdi-eye"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-update" data-bs-toggle="modal" data-bs-target="#update3">
+                                    &nbsp;<i class="mdi mdi-delete"></i>
+                                </a>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://img.wongnai.com/p/1920x0/2021/01/05/1594a014f59246a5b715994cdadad72a.jpg" class="card-img-post" alt="...">
+                    <div class="card-body" id="card-color">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                        <div class="col-sm-12 ">
+                            <center>
+                                <a href="#" class="btn btn-primary btn-update" data-bs-toggle="modal" data-bs-target="#update3">
+                                    &nbsp;<i class="mdi mdi-eye"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-update" data-bs-toggle="modal" data-bs-target="#update3">
+                                    &nbsp;<i class="mdi mdi-delete"></i>
+                                </a>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://d2e5ushqwiltxm.cloudfront.net/wp-content/uploads/sites/63/2016/11/08091339/destination-wat-arun2-700x675.jpg" class="card-img-post" alt="...">
+                    <div class="card-body" id="card-color">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                        <div class="col-sm-12 ">
+                            <center>
+                                <a href="#" class="btn btn-primary btn-update" data-bs-toggle="modal" data-bs-target="#update3">
+                                    &nbsp;<i class="mdi mdi-eye"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-update" data-bs-toggle="modal" data-bs-target="#update3">
+                                    &nbsp;<i class="mdi mdi-delete"></i>
+                                </a>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
