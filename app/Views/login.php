bc6024bca3d59add1f40783f4690693cecee9271
Nutthapon Saefong <73591319+nut1245@users.noreply.github.com>
2022-05-09 00:45:52 +0700

    loginnew

--- app/Views/login.php
+++ app/Views/login.php
@@ -9,13 +9,16 @@
     <!-- plugins:css -->
     <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet">
     <!-- endinject -->
     <!-- Plugin css for this page -->
     <!-- End plugin css for this page -->
     <!-- inject:css -->
     <!-- endinject -->
     <!-- Layout styles -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/login.css">
     <!-- End layout styles -->
     <link rel="shortcut icon" href="images/favicon.png" />
     <!--- sweetalert2 -->
@@ -23,68 +26,108 @@
 </head>
 
 <body>
-    <div class="container-scroller">
-        <div class="container-fluid page-body-wrapper full-page-wrapper">
-            <div class="row w-100 m-0">
 
-                <div class="content-wrapper full-page-wrapper d-flex align-items-center auth login-bg">
-                    <div class="card col-lg-4 mx-auto" >
-
-                        <div class="card-body px-5 py-5" >
-
-                            <h1 class="card-title text-center mb-3" style="color:aliceblue">Login AdminFZ</h1>
-                            <form action="/OfficerController/login" method="post" class="signin-form">
-                                <div class="form-group">
-                                    <?php if (session()->getFlashdata('msg')) : ?>
-                                        <div>
-                                            <script>
-                                                Swal.fire({
-                                                    icon: 'error',
-                                                    text: '<?= session()->getFlashdata('msg') ?>',
-                                                })
-                                            </script>
-                                        <?php endif ?>
-
-                                        <label>ชื่อผู้ใช้งาน</label>
-                                        <input type="text" class="form-control p_input" name="userName" value="<?= set_value('userName'); ?>" required>
-                                        </div>
-                                        <div class="form-group">
-                                            <label>รหัสผ่าน</label>
-                                            <input type="password" class="form-control p_input" name="password" value="<?= set_value('password'); ?>" required>
-
-                                        </div>
-                                        <div class="form-group d-flex align-items-center justify-content-between">
-
-
-                                        </div>
-                                        <div class="text-center">
-                                            <button type="submit" class="btn btn-primary btn-block enter-btn">เข้าสู่ระบบ</button>
-                                        </div>
-
-
-                            </form>
-                        </div>
-                    </div>
+    <div class="center">
+        <div class="ear ear--left"></div>
+        <div class="ear ear--right"></div>
+        <div class="face" action="/OfficerController/login" method="post">
+            <div class="eyes">
+                <div class="eye eye--left">
+                    <div class="glow"></div>
                 </div>
-                <!-- content-wrapper ends -->
+                <div class="eye eye--right">
+                    <div class="glow"></div>
+                </div>
             </div>
-            <!-- row ends -->
+            <div class="nose">
+                <svg width="38.161" height="22.03">
+                    <path d="M2.017 10.987Q-.563 7.513.157 4.754C.877 1.994 2.976.135 6.164.093 16.4-.04 22.293-.022 32.048.093c3.501.042 5.48 2.081 6.02 4.661q.54 2.579-2.051 6.233-8.612 10.979-16.664 11.043-8.053.063-17.336-11.043z" fill="#243946"></path>
+                </svg>
+                <div class="glow"></div>
+            </div>
+            <div class="mouth">
+                <svg class="smile" viewBox="-2 -2 84 23" width="84" height="23">
+                    <path d="M0 0c3.76 9.279 9.69 18.98 26.712 19.238 17.022.258 10.72.258 28 0S75.959 9.182 79.987.161" fill="none" stroke-width="3" stroke-linecap="square" stroke-miterlimit="3"></path>
+                </svg>
+                <div class="mouth-hole"></div>
+                <div class="tongue breath">
+                    <div class="tongue-top"></div>
+                    <div class="line"></div>
+                    <div class="median"></div>
+                </div>
+            </div>
         </div>
-        <!-- page-body-wrapper ends -->
-    </div>
-    <!-- container-scroller -->
-    <!-- plugins:js -->
-    <script src="js/vendor.bundle.base.js"></script>
-    <!-- endinject -->
-    <!-- Plugin js for this page -->
-    <!-- End plugin js for this page -->
-    <!-- inject:js -->
-    <script src="js/off-canvas.js"></script>
-    <script src="js/hoverable-collapse.js"></script>
-    <script src="js/misc.js"></script>
-    <script src="js/settings.js"></script>
-    <script src="js/todolist.js"></script>
-    <!-- endinject -->
+        <div class="hands">
+            <div class="hand hand--left">
+                <div class="finger">
+                    <div class="bone"></div>
+                    <div class="nail"></div>
+                </div>
+                <div class="finger">
+                    <div class="bone"></div>
+                    <div class="nail"></div>
+                </div>
+                <div class="finger">
+                    <div class="bone"></div>
+                    <div class="nail"></div>
+                </div>
+            </div>
+            <div class="hand hand--right">
+                <div class="finger">
+                    <div class="bone"></div>
+                    <div class="nail"></div>
+                </div>
+                <div class="finger">
+                    <div class="bone"></div>
+                    <div class="nail"></div>
+                </div>
+                <div class="finger">
+                    <div class="bone"></div>
+                    <div class="nail"></div>
+                </div>
+            </div>
+        </div>
+        
+        <form action="/OfficerController/login" method="post">
+            <div class="login">
+            <?php if (session()->getFlashdata('msg')) : ?>
+                    <div>
+                        <script>
+                            Swal.fire({
+                                icon: 'error',
+                                text: '<?= session()->getFlashdata('msg') ?>',
+                            })
+                        </script>
+                    <?php endif ?>
+                    <label>
+                        <div class="fa fa-phone"></div>
+                        <input class="username" type="text" autocomplete="on" placeholder="ชื่อผู้ใช้งาน" name="userName" value="<?= set_value('userName'); ?>" required />
+                    </label>
+                    <br>
+                    <label>
+                        <div class="fa fa-commenting"></div>
+                        <input class="password" type="password" autocomplete="off" placeholder="รหัสผ่าน" name="password" value="<?= set_value('password'); ?>" required />
+
+                    </label>
+                    <br>
+                    <button class="login-button" type="submit">เข้าสู่ระบบ</button>
+                    </div>
+        </form>
+
+        <!-- container-scroller -->
+        <!-- plugins:js -->
+        <script src="js/vendor.bundle.base.js"></script>
+        <!-- endinject -->
+        <!-- Plugin js for this page -->
+        <!-- End plugin js for this page -->
+        <!-- inject:js -->
+        <script src="js/off-canvas.js"></script>
+        <script src="js/hoverable-collapse.js"></script>
+        <script src="js/misc.js"></script>
+        <script src="js/settings.js"></script>
+        <script src="js/todolist.js"></script>
+        <script src="js/login.js"></script>
+        <!-- endinject -->
 </body>
 
 </html>
